55221c90f1451b90b8f1dde6a4b11feb6f536a8f
Ahmed Abuelnour <Ahmed.Abuelnoor@outlook.com>
2022-02-19 18:30:32 +0200

    Add project files.

---
+++ JsonLocalizerExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+namespace JsonLocalizer;
+public static class JsonLocalizerExtension
+{
+    public static IServiceCollection AddJsonLocalizer(this IServiceCollection services, string WebRootPath, params CultureInfo[] cultureInfos)
+    {
+        string AcceptLanguage = string.Empty;
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+            options.SupportedCultures = cultureInfos;
+            options.SupportedUICultures = cultureInfos;
+            options.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
+            {
+                var userLangs = context.Request.Headers["Accept-Language"].ToString();
+                var firstLang = userLangs.Split(',').FirstOrDefault();
+                AcceptLanguage = string.IsNullOrEmpty(firstLang) ? "en-US" : firstLang;
+                return await Task.FromResult(new ProviderCultureResult(AcceptLanguage, AcceptLanguage));
+            }));
+        });
+        services.AddScoped(jsonLocalizer =>
+        {
+            return new JsonLocalizerManager(WebRootPath, AcceptLanguage);
+        });
+        return services;
+    }
+}
---
+++ JsonLocalizerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Nodes;
+namespace JsonLocalizer;
+public class JsonLocalizerManager
+{
+    private readonly JsonNode? JsonReader;
+    public JsonLocalizerManager(string WebRootPath, string defaultLang)
+    {
+        JsonReader ??= JsonNode.Parse(json: File.ReadAllText(Path.Combine(WebRootPath, "Resources", $"{defaultLang}.json")));
+    }
+    public string this[string text]
+    {
+        get => JsonReader[text]?.ToString() ?? text;
+    }
+}
+
